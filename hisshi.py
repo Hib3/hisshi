5727c6f9cabe1fff0222e36c9c582cc9a5bc1802
hibiki <hbk@scarecrow.local>
2018-11-20 00:19:31 +0900

    ﾃｽﾄ、とりあえずファイルを書き込めるようにして、整形させようとしたが動かない。ただ、形は悪くないと思うので別ブランチにプッシュ

--- hisshi.py
+++ hisshi.py
@@ -20,45 +20,40 @@ def getURLs():
 
 def getALL():
     getURLs()
-    for thread_title, dat in zip(title, url):
-        print(thread_title,dat)#スレタイとURL
+    for thread_title, dat in zip(title, url):#スレタイとURLが尽きるまで検索
+        #print(thread_title,dat)#スレタイとURL
 
         subject = dat
         request = urllib.request.Request(url=subject, headers=headers)#datとheaderのデータを入れる
 
-        with urllib.request.urlopen(request) as nep:#withを使うことでcloseを省略
-            text = nep.read().decode('cp932')#subject.txtはcp932なので
+        with urllib.request.urlopen(request) as nep:#withを使うことでcloseを省略,接続する。
+            text = nep.read().decode('cp932')#読み込み。subject.txtはcp932なので
 
-            print(text)
-            #ファイルの書き込み
-            if os.path.exists("./hisshi.txt"):
+            #print(text)
+            #ファイルの確認
 
             #with open('./hisshi.txt','a') as file:
-                with open('./'+str(ID)+'.txt','a') as file:  #IDをテキスト名に
-                    if str(ID) in text:
-                        #ここに文字列検索を入れる。もし、IDが見つかったら、スレタイとURLを記載する。textからIDが見つかったら、記入、改行する。
-                        file.write(thread_title)
-                        file.write(dat+str('\n'))
-                        file.write(text)
-                        file.write(str('\n'))
+            with open('./'+str(ID)+'.txt','w') as file:  #IDをテキスト名に
+                if str(ID) in text:
+                    #ここに文字列検索を入れる。もし、IDが見つかったら、スレタイとURLを記載する。textからIDが見つかったら、記入、改行する。
+                    file.write(thread_title)
+                    file.write(dat+str('\n'))
+                    file.write(text)
+                    print(text)
+                    #if str(ID) in text:#textにIDが見つかったら記入して改行する
+                    #    file.write(text)
+                    #    file.write(str('\n'))
+                    #    print(text)
+        #formatter()
+'''
+def formatter():
+    with open('./'+str(ID)+'.txt','r') as file:
+        filedata=file.read()
+        filedata=filedata.replace('<br>','\n')
+    with open('./'+str(ID)+'.txt','w') as file:
+        file.write(filedata)
 
-
-
-            else:
-                #with open('./hisshi.txt','w') as file:
-                with open('./'+str(ID)+'.txt','w') as file:
-
-                    if str(ID) in text:
-                        #ここに文字列検索を入れる。もし、IDが見つかったら、スレタイとURLを記載する。textからIDが見つかったら、記入、改行する。
-                        file.write(thread_title)
-                        file.write(dat+str('\n'))
-                        response = [i for i in text if str(ID) in text]
-                        file.write(response+str('\n'))
-                        #file.write(str('\n'))
-
-            #continue
-        #continue
-
+'''
 
 
 ID = input('記入方法は【ID:OOOOOOO】>>')
